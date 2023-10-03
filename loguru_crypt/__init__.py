d798f6227a21146d489cb74c85a98c22a76ea9e4
VincentGe <cosplox@outlook.com>
2023-10-04 00:50:02 +0800

    update loguru_crypt

---
+++ loguru_crypt/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) CDU
+
+"""Model Docstrings
+
+"""
+
+from __future__ import absolute_import
+from __future__ import annotations
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from loguru_crypt.rsa import RSA
+from loguru_crypt.sink import SimpleEncryptSink
+
+__all__ = ['RSA', 'SimpleEncryptSink']
